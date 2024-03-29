22107eb2a54a0486669d0c512e1385b01caa5758
dd2558 <dd2558@naver.com>
2024-03-29 11:49:02 +0900

    환경설정 완료

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -2,8 +2,11 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ShopApplication {
 
 	public static void main(String[] args) {
