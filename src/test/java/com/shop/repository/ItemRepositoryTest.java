21e7016df336dfedfbaaff7af57c8e58db02db9c
dd2558 <dd2558@naver.com>
2024-04-01 12:27:22 +0900

    QItem추가

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +21,30 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+
+        List<Item> itemList = query.selectFrom(QItem.item)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc())
+                .fetch();
+/*
+		for(Item item : itemList){
+			System.out.println(item.toString());
+		}
+ 		*/
+        itemList.forEach(item -> System.out.println(item));
+
+    }
 
 
     @Test
