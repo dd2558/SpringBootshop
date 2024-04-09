dc9059ac4944548b82b8520088b8d726462110a6
dd2558 <dd2558@naver.com>
2024-04-09 12:48:29 +0900

    지연로딩테스트

--- src/test/java/com/shop/entity/OrderTest.java
+++ src/test/java/com/shop/entity/OrderTest.java
@@ -3,6 +3,7 @@ package com.shop.entity;
 import com.shop.constant.ItemSellStatus;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderItemRepository;
 import com.shop.repository.OrderRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
 import java.time.LocalDateTime;
 
@@ -31,6 +33,26 @@ class OrderTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
+
+    @Test
+    @DisplayName("지연 로딩 테스트")
+    public void lazyLoadingTest(){
+        Order order = this.createOrder();
+        Long orderItemId = order.getOrderItems().get(0).getId();
+        em.flush();
+        em.clear();
+
+        OrderItem orderItem = orderItemRepository.findById(orderItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        System.out.println("Order Class : " + orderItem.getOrder().getClass());
+        System.out.println("====================");
+        orderItem.getOrder().getOrderDate();
+        System.out.println("====================");
+
+    }
 
     public Order createOrder(){
         Order order = new Order();
@@ -55,6 +77,7 @@ class OrderTest {
 
     @Test
     @DisplayName("고아객체 제거 테스트")
+    @Commit
     public void orphanRemovalTest(){
         Order order = this.createOrder();
         order.getOrderItems().remove(0);
@@ -76,6 +99,7 @@ class OrderTest {
 
     @Test
     @DisplayName("영속성 전이 테스트")
+    @Commit
     public void cascadeTest(){
         Order order = new Order();
 
