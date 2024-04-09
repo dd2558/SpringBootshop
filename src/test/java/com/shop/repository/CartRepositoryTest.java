cd3518aa341eef897cb68daf1068af24a59c6fc3
dd2558 <dd2558@naver.com>
2024-04-09 09:57:04 +0900

    Cart Test 구현

---
+++ src/test/java/com/shop/repository/CartRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.shop.repository;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Cart;
+import com.shop.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+class CartRepositoryTest {
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Member createMember(){
+        MemberFormDto memberFormDto = MemberFormDto.builder()
+                .email("test@test.com")
+                .name("홍길동")
+                .address("서울")
+                .password("12345")
+                .build();
+                return Member.createMember(memberFormDto,passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("장바구니 회원 엔티티 매핑 조회 테스트")
+    public void findCartAndMemberTest(){
+        Member member = createMember();
+        memberRepository.save(member);
+
+        Cart cart = new Cart();
+        cart.setMember(member);
+        cartRepository.save(cart);
+
+        em.flush();
+        em.clear();
+
+        Cart savedCart = cartRepository.findById(cart.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        assertEquals(savedCart.getMember().getId(),member.getId());
+    }
+
+}
