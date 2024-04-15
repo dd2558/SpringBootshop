c4f3e7eac3ddc89de4d83c25a8e3ae2d47e51f57
dd2558 <dd2558@naver.com>
2024-04-15 10:07:16 +0900

    메인컨트롤러 수정

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -3,6 +3,7 @@ package com.shop.service;
 import com.shop.dto.ItemFormDto;
 import com.shop.dto.ItemImgDto;
 import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
@@ -82,5 +83,9 @@ public class ItemService {
         return itemRepository.getAdminItemPage(itemSearchDto,pageable);
     }
 
+    @Transactional(readOnly = true)
+    public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable){
+        return itemRepository.getMainItemPage(itemSearchDto,pageable);
+    }
 
 }
